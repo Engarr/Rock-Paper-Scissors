0722ae4c332d640b2ede9715cb1cbe97b3e80756
Łukasz <lukasz.poczta12@gmail.com>
2022-10-11 12:50:55 +0200

    game v 0.8

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 const gameButtons = document.querySelectorAll('.game__item-player');
 const chosenByPlayer = document.querySelector('.player');
 const aiBox = document.querySelector('.box__ai');
-const playerBox = document.querySelector('.box__play');
+const playerBox = document.querySelector('.box__player');
 const result = document.querySelector('.result');
 const retryBtn = document.querySelector('.box__game-btn');
 const gameResult = document.querySelector('.box__game-result-text');
@@ -33,35 +33,49 @@ const pickAi = () => {
 };
 
 const play = (e) => {
-	gameButtons.forEach((btn) => btn.classList.remove('show-buttons-animation'));
 	newPlayerPick = e.target.dataset.pick;
+	gameButtons.forEach((btn) => btn.classList.remove('show-buttons-animation'));
 	pickAi();
-	createAIChose();
-	createPlayerChose(newPlayerPick);
-	result.style.display = 'flex';
+
 	gameButtons.forEach((btn) => btn.classList.add('hide-buttons-animation'));
-	battle()
+	setTimeout(() => {
+		battle();
+	}, 400);
 };
 
 const battle = () => {
+	createPlayerChose(newPlayerPick);
+	createAIChose();
+	result.style.display = 'flex';
+
 	if (newPlayerPick === newAiPick) {
+		playerWeapon.classList.add('rotate-draw-animation');
+
+		aiWeapon.classList.add('rotate-draw-animation');
+		gameResult.classList.add('show-result-animation');
 		gameResult.textContent = 'Draw';
-		playerWeapon.classList.add('draw-animation');
-		aiWeapon.classList.add('draw-animation');
 		drawCount++;
-		draw.textContent = `Draw: ${drawCount}`;
+		setTimeout(() => {
+			draw.textContent = `Draw: ${drawCount}`;
+		}, 1500);
 	} else if (winMap[`${newPlayerPick}`] == newAiPick) {
+		playerWeapon.classList.add('rotate-win-animation');
+		aiWeapon.classList.add('rotate-lose-animation');
+		gameResult.classList.add('show-result-animation');
 		gameResult.textContent = 'You win!';
-		playerWeapon.classList.add('win-animation');
-		aiWeapon.classList.add('lose-animation');
 		winCount++;
-		win.textContent = `Win: ${winCount}`;
+		setTimeout(() => {
+			win.textContent = `Win: ${winCount}`;
+		}, 1500);
 	} else {
-        gameResult.textContent = 'You lose!';
-		playerWeapon.classList.add('lose-animation');
-		aiWeapon.classList.add('win-animation');
+		playerWeapon.classList.add('rotate-lose-animation');
+		aiWeapon.classList.add('rotate-win-animation');
+		gameResult.classList.add('show-result-animation');
+		gameResult.textContent = 'You lose!';
 		loseCount++;
-		lose.textContent = `Lose: ${loseCount}`;
+		setTimeout(() => {
+			lose.textContent = `Lose: ${loseCount}`;
+		}, 1500);
 	}
 };
 
@@ -78,15 +92,14 @@ const createPlayerChose = (newPlayerPick) => {
 	playerWeapon.classList.add('game__item');
 	playerWeapon.classList.add('item');
 	playerWeapon.classList.add(`${newPlayerPick}`);
-
 	playerBox.appendChild(playerWeapon);
 };
 
 const tryAgain = () => {
-	playerBox.innerHTML = '';
-	aiBox.innerHTML = '';
+	playerBox.innerHTML = '<p class="box__player-text">You choiced:</p>';
+	aiBox.innerHTML = '<p class="box__ai-text">Computer choiced:</p>';
 	result.style.display = 'none';
-
+	gameResult.textContent = '';
 	gameButtons.forEach((btn) => btn.classList.remove('hide-buttons-animation'));
 	gameButtons.forEach((btn) => btn.classList.add('show-buttons-animation'));
 };
